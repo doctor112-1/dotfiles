a79eddd18b8a85a96dc331ac275d83dd13a9372a
doctor112-1 <doctor112-1@proton.me>
2026-03-14 15:59:21 -0500

    first commit

---
+++ nvim.bak/.config/nvim.bak/lua/vim-options.lua
@@ -0,0 +1,7 @@
+vim.keymap.set('n', '<leader>qq', ":q!<CR>", {})
+vim.keymap.set('n', '<leader>no', ":!node %<CR>", {})
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
