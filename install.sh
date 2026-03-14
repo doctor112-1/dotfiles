3d75a05efb8202ebc2a854180a95240f20bbc143
doctor112-1 <doctor112-1@proton.me>
2026-03-14 15:59:22 -0500

    neovim install

--- install.sh
+++ install.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
+# general stuff
 sudo pacman -S curl wget git vim base-devel
 
+# install yay
 cd ~ && sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 
+# install a bunch of stuff
 sudo pacman -S sddm stow hyprland hyprlock hyprpaper hypridle xdg-desktop-portal-hyprland hyprshot waybar rofi kitty keychain pipewire wireplumber pipewire-pulse brightnessctl playerctl swaync
-
 yay -S zen-browser-bin
 
-cd ~/dotfiles
+# build and install neovim
+cd ~
+sudo pacman -S base-devel cmake unzip ninja curl
+git clone https://github.com/neovim/neovim
+cd neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
 
+# stow dotfiles
+cd ~/dotfiles
 stow ./hypridle ./hyprland ./backgrounds ./hyprlock ./hyprmocha ./hyprpaper ./kitty ./nvim ./rofi ./starship ./waybar
 
+# setup .bashrc
 echo 'eval "$(starship init bash)"' >>~/.bashrc
 echo 'eval $(keychain --eval --quiet)' >>~/.bashrc
 
+# install nvm, node, and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 nvm install 24
 
+# install pnpm
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 
+# enable sddm
 sudo systemctl enable sddm.service
