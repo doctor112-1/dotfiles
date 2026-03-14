f43a798d37b57c6998e6e680658660f9ec93c61e
doctor112-1 <doctor112-1@proton.me>
2026-03-14 15:59:22 -0500

    folders and btop

--- install.sh
+++ install.sh
@@ -7,8 +7,21 @@ sudo pacman -S curl wget git vim base-devel
 cd ~ && sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 
 # install a bunch of stuff
-sudo pacman -S sddm stow hyprland hyprlock hyprpaper hypridle xdg-desktop-portal-hyprland hyprshot waybar rofi kitty keychain pipewire wireplumber pipewire-pulse brightnessctl playerctl swaync wl-clipboard ripgrep
+sudo pacman -S sddm stow hyprland hyprlock hyprpaper hypridle xdg-desktop-portal-hyprland hyprshot waybar rofi kitty keychain pipewire wireplumber pipewire-pulse brightnessctl playerctl swaync wl-clipboard ripgrep btop
 yay -S zen-browser-bin
+
+# make folder to store screenshots
+mkdir ~/screenshots
+
+# make folder to store videos
+mkdir ~/Videos
+
+# btop
+cd ~
+wget https://github.com/catppuccin/btop/releases/download/1.0.0/themes.tar.gz
+tar xf ~/themes.tar.gz
+mkdir ~/.config/btop
+mv ~/themes ~/.config/btop
 
 # fonts
 sudo pacman -S noto-fonts ttf-jetbrains-mono-nerd
@@ -17,7 +30,7 @@ sudo pacman -S noto-fonts ttf-jetbrains-mono-nerd
 cd ~
 sudo pacman -S base-devel cmake unzip ninja curl
 git clone https://github.com/neovim/neovim
-cd neovim
+cd ~/neovim
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 
@@ -36,6 +49,9 @@ nvm install 24
 
 # install pnpm
 curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+# cleanup
+rm -rf ~/neovim ~/yay ~/themes.tar.gz
 
 # enable sddm
 sudo systemctl enable sddm.service
