2488bd3ad7933162b1e12f668ebdc181b6381d40
doctor112-1 <doctor112-1@proton.me>
2026-04-12 10:44:55 -0500

    hyprlock-dark and hyprlock-light

--- install.sh
+++ install.sh
@@ -45,14 +45,16 @@ theme=$(cat theme)
 if [[ "$theme" = dark ]]; then
   stow -D waybar-light
   stow -D hyprpaper-light
-  stow waybar-dark hyprpaper-dark
+  stow -D hyprlock-light
+  stow waybar-dark hyprpaper-dark hyprlock-dark
   echo "light" >theme
 fi
 
 if [[ "$theme" = light ]]; then
   stow -D waybar-dark
   stow -D hyprpaper-dark
-  stow waybar-light hyprpaper-light
+  stow -D hyprlock-dark
+  stow waybar-light hyprpaper-light hyprlock-light
   echo "dark" >theme
 fi
 
