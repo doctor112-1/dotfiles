cd24e5c356d77d27306764f54f015ec91890d664
doctor112-1 <doctor112-1@proton.me>
2026-06-17 23:37:49 -0500

    remove tmux from auto-launch on .bashrc, and add gentoo-pipewire-launcher to start-dwl.sh

--- config/.bashrc
+++ config/.bashrc
@@ -53,7 +53,3 @@ pnpm() {
 }
 
 PATH=$PATH:~/.local/bin
-
-if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-  exec tmux
-fi
--- start_dwl.sh
+++ start_dwl.sh
@@ -1,1 +1,1 @@
-slstatus -s | dwl -s "bash -c 'swaybg -i $HOME/dotfiles/soft-rose.jpg & widle -t 300 wlock &'"
+slstatus -s | dwl -s "bash -c 'swaybg -i $HOME/dotfiles/soft-rose.jpg & widle -t 300 wlock & gentoo-pipewire-launcher &'"
